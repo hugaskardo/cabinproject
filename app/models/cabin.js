8443dafcbd488e0c184df0331501900e94cc38c3
hugo camacho <hugo.camacho@ipointsystems.com>
2017-03-22 19:43:16 -0600

    Added cabin route, fixed adapters/application.js

--- app/models/cabin.js
+++ app/models/cabin.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+name: DS.attr(),	
   rooms: DS.attr(),
   bathrooms: DS.attr(),
   beds: DS.attr(),
---
+++ mirage/fixtures/cabins.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    name: 'La Arcaica',
+    rooms: 2,
+    bathrooms: 2,
+    beds: 2,
+    livingrooms: 1,
+    capacity: 4
+  }, {
+    name: 'La Bonitinga',
+    rooms: 3,
+    bathrooms: 4,
+    beds: 4,
+    livingrooms: 2,
+    capacity: 6
+  }
+];
